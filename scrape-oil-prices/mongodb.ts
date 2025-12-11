0eccf031616e21a647e582a549160709de081ca0
Todoroscean Dragos <dragoshuniq@gmail.com>
2025-12-11 09:04:14 +0100

    mongo

---
+++ scrape-oil-prices/mongodb.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local",
+  );
+}
+
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      console.log("Database connected");
+
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
+
+export default connectToDatabase;
